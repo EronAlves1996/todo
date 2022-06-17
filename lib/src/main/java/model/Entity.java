60c0092b7e5570ee9ca30995d4446ddb626cc872
Eron Alves <eron.alves@rocketmail.com>
2022-06-17 02:00:54 -0300

    Create Entity class and its methods

---
+++ lib/src/main/java/model/Entity.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Entity {
+	private String name;
+	
+	public Entity(String n) {
+		this.name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
