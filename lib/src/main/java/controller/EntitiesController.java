6501bf9030416295dbec383f290c3ba09d1c7654
Eron Alves <eron.alves@rocketmail.com>
2022-06-19 12:58:38 -0300

    Changed fields and methods in EntitiesController to static

--- lib/src/main/java/controller/EntitiesController.java
+++ lib/src/main/java/controller/EntitiesController.java
@@ -2,23 +2,27 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import model.Entity;
 import com.fasterxml.jackson.databind.*;
 
 public class EntitiesController {
-	public Entity actualEntity;
-	public List<Entity> Entities;
-	ObjectMapper mapper = new ObjectMapper();
+	public static Entity actualEntity;
+	public static List<Entity> entities = new ArrayList<>();
+	private ObjectMapper mapper = new ObjectMapper();
+	private Scanner reader = new Scanner(System.in);
+
 	/**
 	 * Constructor for create the first Entity in a first run of the program
 	 */
-	public EntitiesController(File mainFile) throws IOException {
-		Scanner reader = new Scanner(System.in);
-		System.out.println();
+	public static void addEntity(File mainFile) throws IOException {
 		String entityName = reader.nextLine();
 		actualEntity = new Entity(entityName);
+		entities.add(actualEntity);
 		mapper.writeValue(mainFile, this);
 	}
+	
+	public static void 
 }
